830b2d2bb7f8206a6a249db0a20aad0379385279
Marcus Ebert <mec6975@gmail.com>
2023-01-26 09:42:00 -0800

    changed tpc script

--- roles/xrootd/files/tpc.sh
+++ roles/xrootd/files/tpc.sh
@@ -1,5 +1,18 @@
+<<<<<<< HEAD
+#!/bin/bash
+logfile="/var/log/xrootd/tpc/tpc.log"
+_debug=0
+
+if [ $_debug -eq 1 ];
+then 
+ echo -e "\nDATE: $(date)" | tee -a $logfile 2>&1
+ echo "All args:" | tee -a $logfile 2>&1
+ printf '%s\n' "$*" | tee -a $logfile 2>&1
+fi
+=======
 #!/bin/sh
 set -- `getopt S: -S 1 $*`
+>>>>>>> 0eb421dbf83079683d193848ec5bbdeb6214dc84
 
 OTHERARGS=()
 while [[ $# -gt 0 ]]
@@ -13,6 +26,7 @@ do
     ;;
     *://*)
       SRC="$1"
+<<<<<<< HEAD
       shift
     ;;
     /*)
@@ -23,7 +37,40 @@ do
       OTHERARGS+=("$1")
       shift
     ;;
+=======
+      shift
+    ;;
+    /*)
+      DST="$1"
+      shift
+    ;;
+    *)
+      OTHERARGS+=("$1")
+      shift
+    ;;
+>>>>>>> 0eb421dbf83079683d193848ec5bbdeb6214dc84
   esac
-done
+done >> $logfile 2>&1
 
+if [ $_debug -eq 1 ];
+then 
+ echo "Streams: $STREAMS" | tee -a $logfile 2>&1
+ echo "Source: $SRC" | tee -a $logfile 2>&1
+ echo "Destination: $DST" | tee -a $logfile 2>&1
+ echo "Other Args: ${OTHERARGS[@]}" | tee -a $logfile 2>&1
+fi
+      
+if [[ "$SRC" == *"root"* ]]
+then
+  echo "Running: /usr/bin/xrdcp --server -S $STREAMS $SRC $DST"
+  /usr/bin/xrdcp --server -S $STREAMS $SRC $DST
+elif [[ "$SRC" == *"https"* ]] || [[ "$SRC" == *"davs"* ]]
+then
+ echo "Running: /usr/bin/gfal-copy -n $STREAMS $SRC $DST"
+ /usr/bin/gfal-copy -n $STREAMS $SRC $DST
+fi  >> $logfile 2> >(tee -a $logfile >&2)
+
+<<<<<<< HEAD
+=======
 /bin/xrdcp -d3 --server -S $STREAMS $SRC $DST
+>>>>>>> 0eb421dbf83079683d193848ec5bbdeb6214dc84
