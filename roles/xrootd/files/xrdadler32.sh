221a37dd53f0bffbf631f07c15c67ac90d4f7c78
IceyMu <marcuslee256@gmail.com>
2023-02-01 17:13:59 -0800

    Add subgroups to install different xrootd configurations

--- roles/xrootd/files/xrdadler32.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-file=$1
-davix-get --s3accesskey $AWS_ACCESS_KEY_ID \
-          --s3secretkey $AWS_SECRET_ACCESS_KEY \
-          --s3alternate https://${XRDXROOTD_PROXY}$file | xrdadler32 | awk '{print $1}'
