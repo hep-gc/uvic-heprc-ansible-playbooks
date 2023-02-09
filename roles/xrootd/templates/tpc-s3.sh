63d4c4cce69296f2faeef940aebfa3ae7707ff54
IceyMu <marcuslee256@gmail.com>
2023-02-09 09:55:59 -0800

    Template path for tpc logfile

--- roles/xrootd/templates/tpc-s3.sh
+++ roles/xrootd/templates/tpc-s3.sh
@@ -1,27 +1,30 @@
-#!/bin/bash
-logfile="/var/log/xrootd/standalone/tpc.log"
+#!/bin/sh
+logfile="/var/log/xrootd/{{server_type}}/tpc.log"
 
+echo "+++++++++++++++++++++++++++++++++++++" | tee -a $logfile 2>&1
 echo -e "\nDATE: $(date)" | tee -a $logfile 2>&1
 echo "All args:" | tee -a $logfile 2>&1
 printf '%s\n' "$*" | tee -a $logfile 2>&1
-
 
 OTHERARGS=()
 while [[ $# -gt 0 ]]
 do
   key="$1"
   case $key in
-    -S)
+    -S) 
       STREAMS=$2
+      echo "Wrote $2 to STREAMS" | tee -a $logfile 2>&1
       shift
       shift
     ;;
     *://*)
+      echo "Wrote $1 to SRC" | tee -a $logfile 2>&1
       SRC="$1"
       shift
     ;;
     /*)
       DST="$1"
+      echo "Wrote $1 to DST" | tee -a $logfile 2>&1
       shift
     ;;
     *)
@@ -31,17 +34,12 @@ do
   esac
 done >> $logfile 2>&1
 
-echo "Streams: $STREAMS" | tee -a $logfile 2>&1
+
+echo "Stream: $STREAMS" | tee -a $logfile 2>&1
 echo "Source: $SRC" | tee -a $logfile 2>&1
 echo "Destination: $DST" | tee -a $logfile 2>&1
 echo "Other Args: ${OTHERARGS[@]}" | tee -a $logfile 2>&1
-      
-if [[ "$SRC" == *"root"* ]]
-then
-  echo "Running: /usr/bin/xrdcp --server -S $STREAMS $SRC $DST"
-  xrdcp --server -S $STREAMS $SRC $DST
-elif [[ "$SRC" == *"https"* ]] || [[ "$SRC" == *"davs"* ]]
-then
- echo "Running: /usr/bin/gfal-copy -n $STREAMS $SRC $DST"
- gfal-copy -n $STREAMS $SRC $DST
-fi  >> $logfile 2> >(tee -a $logfile >&2)
+
+echo "xrdcp --server -S $STREAMS $SRC - | s3cmd -c /etc/xrootd/s3cfg put - s3:/$DST" | tee -a $logfile 2>&1
+
+xrdcp --server -S $STREAMS $SRC - | s3cmd -c /etc/xrootd/s3cfg put - s3:/$DST
