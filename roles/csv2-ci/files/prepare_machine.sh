74c79e345e5b6ffd8e9ef6f2ad19ed1ed8bb9190
MarcusEbert <mec6975@gmail.com>
2023-09-06 11:56:32 -0700

    Update prepare_machine.sh
    auto discover of hostname added

--- roles/csv2-ci/files/prepare_machine.sh
+++ roles/csv2-ci/files/prepare_machine.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-ip=$(curl ipinfo.io/ip 2>/dev/null|cut -d. -f4)
-host_name=elephant$ip.heprc.uvic.ca
+yum install -y bind-utils
+ip=$(curl ipinfo.io/ip 2>/dev/null)
+host_name=$(nslookup $ip|grep name|cut -d= -f2|sed 's/.$//')
 #echo "Host:$1"
 setenforce 0
 sed -i 's/enforcing/disabled/g;' /etc/sysconfig/selinux 
