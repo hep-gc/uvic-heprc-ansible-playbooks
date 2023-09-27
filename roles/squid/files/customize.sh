557af65a30d1bdee0d243ce2b908b1527d973715
Alyssa Blair <alyssablair@heplw62.phys.uvic.ca>
2023-09-27 15:06:44 -0700

    add configuration with rock cache

--- roles/squid/files/customize.sh
+++ roles/squid/files/customize.sh
@@ -14,6 +14,11 @@ awk --file `dirname $0`/customhelps.awk --source '{
 setoption("acl NET_LOCAL src", "XYZZYX")
 uncomment("acl MAJOR_CVMFS")
 uncomment("acl ATLAS_FRONTIER")
+setoption("cache_mem", "CACHE_MEM")
+setoption("maxiumum_object_size_in_memory", "1 GB")
+setoption("cache_dir", "rock /var/cache/squid CACHE_DIR")
+setoption("memory_cache_shared", "on")
+setoption("workers", VCORE)
 insertline("^# http_access deny !RESTRICT_DEST", "http_access allow ATLAS_FRONTIER")
 insertline("^# http_access deny !RESTRICT_DEST", "http_access deny !MAJOR_CVMFS")
 print
