f15800755c51125862cda9ea317f1ed923866f43
Alyssa Blair <alyssablair@heplw62.phys.uvic.ca>
2023-09-28 11:17:46 -0700

    update template

--- roles/squid/templates/customize.sh
+++ roles/squid/templates/customize.sh
@@ -11,12 +11,12 @@
 #
 
 awk --file `dirname $0`/customhelps.awk --source '{
-setoption("acl NET_LOCAL src", "IP")
+setoption("acl NET_LOCAL src", "{{IP.stdout}}.0.0/16")
 uncomment("acl MAJOR_CVMFS")
 uncomment("acl ATLAS_FRONTIER")
-setoption("cache_mem", "CACHE_MEM")
-setoption("cache_dir", 3, "CACHE_DIR")
-setoption("workers", VCORE)
+setoption("cache_mem", "{{CACHE_MEM}}")
+setoption("cache_dir", 3, "{{CACHE_DIR}}")
+setoption("workers", {{VCORE.stdout}})
 setoptionparameter("cache_dir", 2, "/var/cache/squid/squid${process_number}")
 setoption("cpu_affinity_map", "process_numbers=1,2,3,4,5,6,7 cores=1,2,3,4,5,6,7")
 insertline("^acl MAJOR_CVMFS", "acl BELLEII dstdom_regex ^(cvmfs-stratum-one.cc.kek.jp)$ ")
