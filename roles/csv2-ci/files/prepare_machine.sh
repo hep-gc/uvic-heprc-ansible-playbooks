3759eba5075581f75e183b20da08de1ef18f2ff1
Travis Lee <travislee2k15@gmail.com>
2024-11-19 10:49:26 -0800

    added firewall rules in perparemachine for csv2 ci target deploys

--- roles/csv2-ci/files/prepare_machine.sh
+++ roles/csv2-ci/files/prepare_machine.sh
@@ -3,13 +3,14 @@
 host_name=$(curl ipinfo.io 2>/dev/null|grep hostname|cut -d'"' -f4)
 #echo "Host:$1"
 setenforce 0
-sed -i 's/enforcing/disabled/g;' /etc/sysconfig/selinux 
-sed -i 's/enforcing/disabled/g;' /etc/selinux/config
+sed -i 's/enforcing/permissive/g;' /etc/sysconfig/selinux 
+sed -i 's/enforcing/permissive/g;' /etc/selinux/config
 yum install -y firewalld
 sed -i 's/#Port 22/Port 22/g;' /etc/ssh/sshd_config 
 systemctl restart sshd
 echo $host_name >/etc/hostname 
 hostname $host_name
 systemctl enable firewalld && systemctl restart firewalld
+firewall-cmd --permanent --zone=public --add-service=http && firewall-cmd --permanent --zone=public --add-service=https && firewall-cmd --reload
 
 
