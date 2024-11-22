fb225c8ca0a359c9541d9549067b3fc224ba3127
Travis Lee <travislee2k15@gmail.com>
2024-11-22 11:41:39 -0800

    moved firewall rules back to associated tasks. Removed firewall rules from prepare_machine.sh. Removed install_configure_firewalld. Changed failure and change modes for adding users using cloudscheduler commands.

--- roles/csv2-ci/files/prepare_machine.sh
+++ roles/csv2-ci/files/prepare_machine.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
 host_name=$(curl ipinfo.io 2>/dev/null|grep hostname|cut -d'"' -f4)
-#echo "Host:$1"
+echo "Host:$1"
 setenforce 0
 sed -i 's/enforcing/disabled/g;' /etc/sysconfig/selinux 
 sed -i 's/enforcing/disabled/g;' /etc/selinux/config
-yum install -y firewalld
+dnf install -y firewalld
 sed -i 's/#Port 22/Port 22/g;' /etc/ssh/sshd_config 
 systemctl restart sshd
 echo $host_name >/etc/hostname 
 hostname $host_name
 systemctl enable firewalld && systemctl restart firewalld
-firewall-cmd --permanent --zone=public --add-service=http 
-firewall-cmd --permanent --zone=public --add-service=https
-firewall-cmd --permanent --zone=public --add-port=8086/tcp
-firewall-cmd --permanent --zone=public --add-port=9618/tcp
-firewall-cmd --permanent --zone=public --add-port=40000-50000/tcp
-firewall-cmd --reload
